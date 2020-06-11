7dcba3ebe2092889af43bd90f9e033b70705f903
la1314 <44264388+la1314@users.noreply.github.com>
2020-06-11 12:46:52 +0200

    Añadidos nuevos colores, ahora el header representa el boton seleccionado

--- src/components/mainApp/header/EditorHeader.jsx
+++ src/components/mainApp/header/EditorHeader.jsx
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 
 export default class EditorHeader extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            select: 0,
+            clsname: 'select'
+        }
+
+    }
+
+    changeSelect = () => {
+        if (localStorage.getItem('page')) {
+            const n = parseInt(localStorage.getItem('page'));
+            this.setState({select: n})
+        } 
+    }
+
     render() {
+
+        const {select, clsname} = this.state;
+
         return (
             <div className='header-app'>
                 <div className='menu-item-container'>
-                    <MenuItem className='menu-item' nameItem='Gestor' onClick={() => this.props.changePage(0)} />
-                    <MenuItem className='menu-item' nameItem='Biblioteca' onClick={() => this.props.changePage(1)} />
-                    <MenuItem className='menu-item' nameItem='Perfil' onClick={() => this.props.changePage(2)} />
+                    <MenuItem className={`menu-item ` + (select===0 && clsname) } nameItem='Gestor' onClick={() => Promise.resolve(this.props.changePage(0)).then( () => {this.changeSelect()} ) } />
+                    <MenuItem className={'menu-item ' + (select===1 && clsname) } nameItem='Biblioteca' onClick={() => Promise.resolve(this.props.changePage(1)).then( () => {this.changeSelect()} ) } />
+                    <MenuItem className={'menu-item ' + (select===2 && clsname) } nameItem='Perfil' onClick={() => Promise.resolve(this.props.changePage(2)).then( () => {this.changeSelect()} ) } />
                     <MenuItem className="menu-item-logout" nameItem='' onClick={() => this.props.logout()} />
                 </div>
                
--- src/components/mainApp/header/UserHeader.jsx
+++ src/components/mainApp/header/UserHeader.jsx
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 
 export default class UserHeader extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            select: 0,
+            clsname: 'select'
+        }
+
+    }
+
+    changeSelect = () => {
+        if (localStorage.getItem('page')) {
+            const n = parseInt(localStorage.getItem('page'));
+            this.setState({select: n})
+        } 
+    }
+
+
     render() {
+
+        const {select, clsname} = this.state;
+
         return (
             <div className='header-app'>
                 <div className='menu-item-container'>
-                    <MenuItem className='menu-item' nameItem='Home' onClick={() => this.props.changePage(0)} />
-                    <MenuItem className='menu-item' nameItem='Biblioteca' onClick={() => this.props.changePage(1)} />
-                    <MenuItem className='menu-item' nameItem='Perfil' onClick={() => this.props.changePage(2)} />
+                    <MenuItem className={'menu-item ' + (select===0 && clsname) } nameItem='Home' onClick={() => Promise.resolve(this.props.changePage(0)).then( () => {this.changeSelect()} ) } />
+                    <MenuItem className={'menu-item ' + (select===1 && clsname) } nameItem='Biblioteca' onClick={() => Promise.resolve(this.props.changePage(1)).then( () => {this.changeSelect()} )} />
+                    <MenuItem className={'menu-item ' + (select===2 && clsname) } nameItem='Perfil' onClick={() => Promise.resolve(this.props.changePage(2)).then( () => {this.changeSelect()} ) } />
                     <MenuItem className="menu-item-logout" nameItem='' onClick={() => this.props.logout()} />
                 </div>
-               
             </div>
         );
     }
